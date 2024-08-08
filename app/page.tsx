b92757d10efda2cd04630c6a4ca4ae5db7d57700
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-08-08 21:24:51 +0900

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from '../styles/home.module.css'
+import Person from '../components/person'
 
-function Home() {
+async function getBillionaires() {
+  try {
+    return (await fetch("https://billions-api.nomadcoders.workers.dev/")).json()
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+
+async function Home() {
+  const billionaires = await getBillionaires()
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+        {billionaires.map(person =>
+          <Person key={person.id}
+            {...person}
+          />)}
+    </div>
   )
 }
 
